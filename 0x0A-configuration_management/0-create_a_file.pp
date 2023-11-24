a562f4c90d160ea51803a06f13c4dfc1a348506a
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-11-24 23:34:41 +0000

    Add ensure => file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,11 @@
 # Create a file in /tmp
 
 file {'/tmp/school':
-    content  => 'I love Puppet',
-    mode  => '0744',
+    ensure => file,
     path  => '/tmp/school',
+    mode  => '0744',
     owner => 'www-data',
-    group => 'www-data'
+    group => 'www-data',
+    content  => 'I love Puppet',
+
 }
