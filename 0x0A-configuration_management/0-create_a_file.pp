46f533ffd28b7a585ee7a3f7163656b2a16f52fe
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-11-24 23:28:34 +0000

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file in /tmp
+
+file {'/tmp/school':
+    content  => 'I love Puppet',
+    mode  => '0744',
+    path  => '/tmp/school',
+    owner => 'www-data',
+    group => 'www-data'
+}
