807c3b7a91f2064d03949fd55b966a203926b14e
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-11-25 00:24:57 +0000

    Revert changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,6 @@
 # Install flask from pip3 using Puppet
 
-# Ensure that pip3 is installed
-package { 'python3-pip':
-  ensure => installed,
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3'
 }
-
-# Install Flask using pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask',
-  path    => '/usr/bin',
-  require => Package['python3-pip'],
-}
-
