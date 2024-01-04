fee32a7b717ff2af7ffb4203e92e7a6083186e12
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-01-04 17:39:58 +0000

    Fix code to pass pycodestyle

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -8,18 +8,26 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
+    USER_TASKS = []
+
+    # Make an API call to users endpoint
     users = requests.get('https://jsonplaceholder.typicode.com/users')
+    # Loop through all users until argv[1] matches user id
     for user in users.json():
         if user.get('id') == int(argv[1]):
             EMPLOYEE_USERNAME = user.get('username')
             break
-    USER_TASKS = []
+
+    # Make an API call to todos endpoint
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    # Loop through todos until argv[1] matches todo UserId
     for todo in todos.json():
         if todo.get('userId') == int(argv[1]):
-            USER_TASKS.append({"task": todo.get('title'), "completed": todo.get('completed'), "username": EMPLOYEE_USERNAME})
-    
+            USER_TASKS.append({
+                "task": todo.get('title'),
+                "completed": todo.get('completed'),
+                "username": EMPLOYEE_USERNAME})
+
     filename = "{}.json".format(argv[1])
     with open(filename, 'w') as JSON_file:
         json.dump({argv[1]: USER_TASKS}, JSON_file)
-#{ "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"},
