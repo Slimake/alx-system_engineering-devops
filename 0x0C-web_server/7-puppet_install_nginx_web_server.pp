bb4c3cd1b96de45edd01477db6371b0bd447bd3a
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-07-02 15:44:39 +0100

    Configuring a server with Puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,7 +2,6 @@
 # Also perform a 301 redirect when querying /redirect_me.
 
 $add301_redirect = '\\\n\tlocation /redirect_me {\n\t\t return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}'
-$add404_notfound = '\\\n\terror_page 404 /custom_404.html;\n\tlocation = /custom_404.html {\n\t\tinternal;\n\t}'
 
 package { 'install_and_configure_nginx':
     ensure => 'installed',
@@ -14,18 +13,8 @@ file { '/var/www/html/index.nginx-debian.html':
     content => 'Hello World!',
 }
 
-file { '/var/www/html/custom_404.html':
-    ensure  => file,
-    content => 'Ceci n\'est pas une page',
-}
-
 exec { 'add_301_redirect':
     command => "sed -i '53i ${add301_redirect}' /etc/nginx/sites-available/default",
-    path    => '/usr/bin:/usr/sbin:/bin',
-}
-
-exec { 'add_404_notfound':
-    command => "sed -i '57i ${add404_notfound}' /etc/nginx/sites-available/default",
     path    => '/usr/bin:/usr/sbin:/bin',
 }
 
