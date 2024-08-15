aa18be2e55200685db7e5c12b36a24d989a63279
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-08-15 18:42:21 +0100

    Add comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# Using strace, find out why Apache is returning a 500 error.
+# Once you find the issue, fix it and then automate it using Puppet
 exec { 'fix-wordpress':
   command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' wp-settings.php",
   cwd     => '/var/www/html',
